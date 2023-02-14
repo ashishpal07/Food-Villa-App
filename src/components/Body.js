df3919294ea87a51724e9b7b58b1ca19d4003082
ashishpal07 <ashishpal7861@gmail.com>
2023-02-14 15:07:04 +0530

    created search container and learned about useState in react

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,42 @@
 
+import { useState } from "react";
 import { restaurentList } from "../constants";
 import ReastaurentCart from "./ReastaurentCart";
+import { useState } from "react";
+
+// in react every component has a state
+// what is state
+// what is hooks?
+// what is useState
 
 const Body = () => {
+    // const searchText = "KFC"; This is how we create normal variable with default value
+    // searchText is local state variable
+    const [searchText, setSearchText] = useState("KFC");  // To create state variables with default value
+    const [searchClicked, setSearchClicked] = useState("false");
+
     return (
         <>
             <div className="search-container">
-                <input type="text" className="search-input" placeholder="search" value="" />
-                <button className="search-btn">Search</button>
+                <input 
+                    type="text" 
+                    className="search-input" 
+                    placeholder="search" 
+                    value={searchText}
+                    onChange={(e) => {
+                        setSearchText(e.target.value);
+                    }}
+                />
+                <h1>{searchClicked}</h1>
+                <button 
+                className="search-btn" 
+                onClick={() => {
+                    if(searchClicked === "false") {
+                        setSearchClicked("true");
+                    } else {
+                        setSearchClicked("false");
+                    }
+                }}>Search</button>
             </div>
 
             <div className="restaurent-list">
